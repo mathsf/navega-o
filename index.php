38ec82a72d71f53e2074a8fcfeda3238740edd10
mathsf <109424595+mathsf@users.noreply.github.com>
2022-10-09 11:25:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,23 @@
     <meta name="viewport" content="width=device-width,
     initial-scale=1, minimum-scale=1">
     <title></title>
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="stylee.css">
 </head>
 
 <body>
     <header>
         <nav> 
-            <a href="/" class="logo"><i><b>Kastelar</b></i></a>
+            <a href="#" class="logo"><i><b>Kastelar</b></i></a>
             <div class="menu-movel">
                 <div class="linha1"></div>
                 <div class="linha2"></div>
                 <div class="linha3"></div>
             </div>
             <ul class="nave-lista">
-                <li><a href="/">Início</a></li>
-                <li><a href="/">Sobre</a></li>
-                <li><a href="/">Projetos</a></li>
-                <li><a href="/">Contatos</a></li>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contatos</a></li>
             </ul>
         </nav>
     </header>
