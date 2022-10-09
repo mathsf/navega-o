fd4f6007d8abbbe863757ab4d880aa72fd2b6eb1
mathsf <109424595+mathsf@users.noreply.github.com>
2022-10-09 11:06:09 -0300

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1, minimum-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <header>
+        <nav> 
+            <a href="/" class="logo"><i><b>Kastelar</b></i></a>
+            <div class="menu-movel">
+                <div class="linha1"></div>
+                <div class="linha2"></div>
+                <div class="linha3"></div>
+            </div>
+            <ul class="nave-lista">
+                <li><a href="/">Início</a></li>
+                <li><a href="/">Sobre</a></li>
+                <li><a href="/">Projetos</a></li>
+                <li><a href="/">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <script src="jbarramobile.js"></script>
+    </main>
+</body>
+
+</html>
